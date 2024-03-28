97872c1f4d56a8fbe5db029799d91c0a4640a9cb
theshaozhiyuan <theshaozhiyuan@gmail.com>
2024-03-28 15:09:22 -0400

    show timestamp
    show timestamp

--- app/src/main/java/com/example/myapplication/InfoViewHolder.java
+++ app/src/main/java/com/example/myapplication/InfoViewHolder.java
@@ -11,12 +11,14 @@ public class InfoViewHolder extends RecyclerView.ViewHolder{
     public TextView senderName;
     public TextView receiverName;
     public ImageView message;
+    public TextView time;
 
     public InfoViewHolder(@NonNull View itemView) {
         super(itemView);
         this.senderName = itemView.findViewById(R.id.textViewSenderName);
         this.receiverName = itemView.findViewById(R.id.textViewRecipientName);
         this.message = itemView.findViewById(R.id.imageViewPicture);
+        this.time = itemView.findViewById(R.id.textViewTime);
     }
 
 
